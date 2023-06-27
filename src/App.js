9a6e8979b3b8de83441f3b59c24a367b51fe76fb
SaidulIslam16 <isaidulislamsayed@gmail.com>
2023-06-27 21:57:12 +0600

    inplemented a router name restaurant and setup the 404 page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
+
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import './App.css';
 
 function App() {
+  const restaurant = createBrowserRouter([
+    { path: '/', element: <div>Hello</div> },
+    { path: '*', element: <div><h1>This is 404 Not Found Page</h1></div> }
+  ]);
   return (
     <div className="App">
-
+      <RouterProvider router={restaurant}></RouterProvider>
     </div>
   );
 }
